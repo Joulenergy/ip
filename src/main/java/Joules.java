cff7b8e6467c53b1502ad4645226edd5e94086c0
Joulenergy <118324748+Joulenergy@users.noreply.github.com>
2025-08-22 13:01:54 +0800

    Echo

--- src/main/java/Joules.java
+++ src/main/java/Joules.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Joules {
     public static void main(String[] args) {
         System.out.println(" Hello! I'm Joules!\n" +
                 " What can I do for you?");
-        System.out.println(" Bye. Hope to see you again soon!");
+
+        // Accept user input
+        Scanner scanner = new Scanner(System.in);
+        boolean bye = false;
+        while (!bye) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                bye = true;
+                System.out.println(" Bye. Hope to see you again soon!");
+            } else {
+                System.out.println(" " + input);
+            }
+        }
     }
 }
