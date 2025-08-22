6a9f4e2c8bb5e6ff35d2adf94227743b89ec863b
Joulenergy <118324748+Joulenergy@users.noreply.github.com>
2025-08-22 13:11:27 +0800

    Add list

--- src/main/java/Joules.java
+++ src/main/java/Joules.java
@@ -5,6 +5,10 @@ public class Joules {
         System.out.println(" Hello! I'm Joules!\n" +
                 " What can I do for you?");
 
+        // Store input
+        String[] history = new String[100];
+        int index = 0;
+
         // Accept user input
         Scanner scanner = new Scanner(System.in);
         boolean bye = false;
@@ -13,8 +17,14 @@ public class Joules {
             if (input.equals("bye")) {
                 bye = true;
                 System.out.println(" Bye. Hope to see you again soon!");
+            } else if (input.equals("list")) {
+                for (int i = 0; i < index; i++) {
+                    System.out.printf(" %d. %s%n", i + 1, history[i]);
+                }
             } else {
-                System.out.println(" " + input);
+                history[index] = input;
+                index += 1;
+                System.out.println(" added: " + input);
             }
         }
     }
